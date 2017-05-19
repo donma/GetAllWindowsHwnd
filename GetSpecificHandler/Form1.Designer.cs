05b140998245b4549d3139809f49dd3339d37f2a
Donma Stark <Donma Stark>
2017-05-19 13:42:48 +0800

    加入專案檔案。

---
+++ GetSpecificHandler/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace GetSpecificHandler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 27;
+            this.listBox1.Location = new System.Drawing.Point(-1, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(633, 542);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(-1, 548);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(633, 72);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "取得所有hWnd我可以抓到的";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(-1, 626);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(633, 72);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "抓Chrome";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(635, 700);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("新細明體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sample";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ GetSpecificHandler/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetSpecificHandler
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern int EnumWindows(CallbackDef callback, int lParam);
+
+      
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(int hWnd, StringBuilder text, int count);
+
+        delegate bool CallbackDef(int hWnd, int lParam);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private bool ShowWindowHandler(int hWnd, int lParam)
+        {
+
+            string mystring;
+
+            StringBuilder text = new StringBuilder(255);
+            GetWindowText(hWnd, text, 255);
+
+            mystring = text.ToString();
+            listBox1.Items.Insert(0, text + "," + Convert.ToString((int)hWnd, 16).ToUpper());
+
+            return true;
+
+        }
+
+
+        private bool ShowIEWindowHandler(int hWnd, int lParam)
+        {
+
+            string mystring;
+
+            StringBuilder text = new StringBuilder(255);
+            GetWindowText(hWnd, text, 255);
+
+            mystring = text.ToString();
+            if(mystring.Contains("Chrome"))
+            listBox1.Items.Insert(0, text + "," + Convert.ToString((int)hWnd, 16).ToUpper());
+
+            return true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            CallbackDef callback = new CallbackDef(ShowWindowHandler);
+            EnumWindows(callback, 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            CallbackDef callback = new CallbackDef(ShowIEWindowHandler);
+            EnumWindows(callback, 0);
+        }
+    }
+}
